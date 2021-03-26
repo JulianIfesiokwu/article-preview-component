b6569f011d30d2e321e23b8c79c30161b68e5334
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-03-26 14:50:11 +0100

    started working on dektop view css

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,24 @@ const shareIcon = document.querySelector('.share');
 const about = document.querySelector('.about');
 const socialLinks = document.querySelector('.social-links');
 const returnBtn = document.querySelector('.icons-share');
+const information = document.querySelector('.information');
 
 // functions
 function toggleDisplayMedia() {
     if(about.style.display === 'flex') {
         about.style.display = 'none';
+        information.classList.add('no-padding-bottom')
         socialLinks.style.display = 'flex';
     } else {
         about.style.display = 'flex';
         socialLinks.style.display = 'none';
+        information.classList.remove('no-padding-bottom');
     }
 }
-
-
-
 
 // event listeners
 shareIcon.addEventListener('click', toggleDisplayMedia);
 returnBtn.addEventListener('click', toggleDisplayMedia);
-
 
 // at startup
 toggleDisplayMedia();
